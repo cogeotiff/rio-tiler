3ff380e829fcfe9a01c21ab0c62507273c9d7dfb
vincentsarago <vincent.sarago@mapbox.com>
2018-02-03 22:25:28 -0500

    add alpha, nodata and internal mask support, refactor one band image support and return mask data

--- rio_tiler/utils.py
+++ rio_tiler/utils.py
@@ -17,7 +17,8 @@ from rasterio.enums import Resampling
 from rasterio.plot import reshape_as_image
 from rio_toa import reflectance, brightness_temp, toa_utils
 
-from rio_tiler.errors import (InvalidFormat,
+from rio_tiler.errors import (RioTilerError,
+                              InvalidFormat,
                               InvalidLandsatSceneId,
                               InvalidSentinelSceneId,
                               InvalidCBERSSceneId)
@@ -117,19 +118,21 @@ def band_min_max_worker(address, pmin=2, pmax=98, width=1024, height=1024):
     return np.percentile(arr[arr > 0], (pmin, pmax)).astype(np.int).tolist()
 
 
-def tile_band_worker(address, bounds, tilesize, indexes=[1], nodata=0):
+def tile_band_worker(address, bounds, tilesize, indexes=[1], nodata=None, alpha=None):
     """Read band data
 
     Attributes
     ----------
 
-    address : Sentinel-2/Landsat-8 band AWS address
+    address : file address
     bounds : Mercator tile bounds to retrieve
     tilesize : Output image size
     indexes : list of ints or a single int, optional, (default: 1)
         If `indexes` is a list, the result is a 3D array, but is
         a 2D array if it is a band index number.
-    nodata: int or float, optional (defaults: 0)
+    nodata: int or float, optional (defaults: None)
+    alpha: int, optional (defaults: None)
+        Force alphaband if not present in the dataset metadata
 
     Returns
     -------
@@ -138,26 +141,41 @@ def tile_band_worker(address, bounds, tilesize, indexes=[1], nodata=0):
     """
     w, s, e, n = bounds
 
-    out_shape = (tilesize, tilesize)
+    if alpha is not None and nodata is not None:
+        raise RioTilerError('cannot pass alpha and nodata option')
 
-    if not isinstance(indexes, int):
-        if len(indexes) == 1:
-            indexes = indexes[0]
-        else:
-            out_shape = (len(indexes),) + out_shape
+    if isinstance(indexes, int):
+        indexes = [indexes]
+
+    out_shape = (len(indexes), tilesize, tilesize)
 
     with rasterio.open(address) as src:
-        with WarpedVRT(src,
-                       dst_crs='EPSG:3857',
-                       resampling=Resampling.bilinear,
-                       src_nodata=nodata,
-                       dst_nodata=nodata) as vrt:
+        with WarpedVRT(src, dst_crs='EPSG:3857', resampling=Resampling.bilinear,
+                       src_nodata=nodata, dst_nodata=nodata) as vrt:
+
                             window = vrt.window(w, s, e, n, precision=21)
-                            return vrt.read(window=window,
+
+                            data = vrt.read(window=window,
                                             boundless=True,
                                             resampling=Resampling.bilinear,
                                             out_shape=out_shape,
                                             indexes=indexes)
+
+                            mask = vrt.read_masks(1, window=window,
+                                                  out_shape=(tilesize, tilesize),
+                                                  boundless=True,
+                                                  resampling=Resampling.bilinear)
+
+                            if nodata:
+                                mask = np.all(data != nodata, axis=0).astype(np.uint8) * 255
+
+                            if alpha:
+                                mask = vrt.read(alpha, window=window,
+                                                out_shape=(tilesize, tilesize),
+                                                boundless=True,
+                                                resampling=Resampling.bilinear)
+
+    return data, mask
 
 
 def linear_rescale(image, in_range=(0, 1), out_range=(1, 255)):
