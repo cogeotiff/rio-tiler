e034465ce78d41e963c6440f8436db0b2865a425
vincentsarago <vincent.sarago@gmail.com>
2019-07-16 11:01:29 -0400

    add colormap listing and fix schwarzwald colormap type

--- rio_tiler/cmap/__init__.py
+++ rio_tiler/cmap/__init__.py
@@ -17,4 +17,27 @@ Additional colormaps:
 - rplumbo: https://github.com/cogeotiff/rio-tiler/pull/90
 - schwarzwald: http://soliton.vm.bytemark.co.uk/pub/cpt-city/wkp/schwarzwald/tn/wiki-schwarzwald-cont.png.index.html
 
+Code inspired from our friends https://github.com/DHI-GRAS/terracotta/blob/master/terracotta/cmaps/__init__.py
 """
+
+import os
+from pkg_resources import resource_listdir, Requirement, DistributionNotFound
+
+
+SUFFIX = ".npy"
+
+try:
+    PACKAGE = Requirement.parse("rio_tiler")
+    cmap_list = resource_listdir(PACKAGE, "rio_tiler/cmap")
+    cmap_list = [
+        os.path.splitext(os.path.basename(f))[0]
+        for f in cmap_list
+        if f.endswith(SUFFIX)
+    ]
+except DistributionNotFound:  # rio_tiler was not installed, fall back to file system
+    PACKAGE_DIR = os.path.dirname(__file__)
+    cmap_list = [
+        os.path.splitext(os.path.basename(f))[0]
+        for f in os.listdir(PACKAGE_DIR)
+        if f.endswith(SUFFIX)
+    ]
