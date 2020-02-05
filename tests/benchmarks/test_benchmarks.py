4fe0eb48eeff579d8fa99aa0b4cdab7f15d2e42c
vincentsarago <vincent.sarago@gmail.com>
2020-02-05 10:27:12 -0500

    update fixtures factory

--- tests/benchmarks/test_benchmarks.py
+++ tests/benchmarks/test_benchmarks.py
@@ -21,23 +21,18 @@ def read_tile(src_path, tile):
             return utils._tile_read(src_dst, tile_bounds, 256)
 
 
-def test_create_bench_dataset(benchmark, benchmark_fixtures):
-    """Create Benchmark Dataset."""
-    pass
-
-
 @pytest.mark.parametrize("tile_name", ["full", "boundless"])
 @pytest.mark.parametrize("dataset_info", benchmark_dataset)
-def test_tile(benchmark, dataset_info, tile_name):
+def test_tile(benchmark, dataset_info, tile_name, cloudoptimized_geotiff):
     """Test tile read for multiple combination of datatype/mask/tile extent."""
+    src_path = cloudoptimized_geotiff(cog_path, **dataset_info)
+
     name = dataset_info["name"]
     dtype = dataset_info["dtype"]
     nodata_type = dataset_info["nodata_type"]
-    nbands = dataset_info.get("nbands", 1)
 
     benchmark.name = "{}-{}".format(dtype, nodata_type)
     benchmark.group = "{} tile ".format(tile_name)
 
-    fin = "{}/{}-{}-{}-{}b.tif".format(cog_path, name, dtype, nodata_type, nbands)
     tile = benchmark_tiles[name][tile_name]
-    data, mask = benchmark(read_tile, fin, tile)
+    data, mask = benchmark(read_tile, src_path, tile)
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ import pytest
 
 import numpy
 
-import rasterio
 from rasterio.io import MemoryFile
 from rasterio.transform import from_bounds
 from rasterio.enums import ColorInterp
@@ -14,104 +13,86 @@ from rasterio.enums import ColorInterp
 from rio_cogeo.cogeo import cog_translate
 from rio_cogeo.profiles import cog_profiles
 
-with rasterio.Env() as env:
-    drivers = env.drivers()
 
+@pytest.fixture
+def cloudoptimized_geotiff():
+    """Create CloudOptimized GeoTIFF fixture."""
 
-requires_webp = pytest.mark.skipif(
-    "WEBP" not in drivers.keys(), reason="Only relevant if WEBP drivers is supported"
-)
+    def _cloudoptimized_geotiff(
+        output_dir,
+        name,
+        crs,
+        bounds,
+        dtype,
+        nodata_type,
+        tilesize=256,
+        nband=1,
+        x_size=2000,
+        y_size=2000,
+    ):
+        fout = "{}/{}-{}-{}-{}b.tif".format(output_dir, name, dtype, nodata_type, nband)
+        if os.path.exists(fout):
+            return fout
 
+        profile_options = {"blockxsize": tilesize, "blockysize": tilesize}
+        output_profile = cog_profiles.get("deflate")
+        output_profile.update(profile_options)
 
-def create_cog(
-    output_dir,
-    name,
-    crs,
-    bounds,
-    dtype,
-    nodata_type,
-    tilesize=256,
-    nband=1,
-    x_size=2000,
-    y_size=2000,
-):
-    """Create COG fixture."""
-    fout = "{}/{}-{}-{}-{}b.tif".format(output_dir, name, dtype, nodata_type, nband)
-    if os.path.exists(fout):
+        arr = numpy.random.randint(1, 255, size=(nband, y_size, x_size)).astype(
+            numpy.uint8
+        )
+        arr[:, 0:500, 0:500] = 0
+
+        mask = numpy.zeros((1, y_size, x_size), dtype=numpy.uint8) + 255
+        mask[:, 0:500, 0:500] = 0
+
+        w, s, e, n = bounds
+        src_profile = dict(
+            driver="GTiff",
+            count=nband,
+            dtype="uint8",
+            height=y_size,
+            width=x_size,
+            crs=crs,
+            transform=from_bounds(w, s, e, n, x_size, y_size),
+        )
+        if nodata_type in ["nodata", "mask"]:
+            src_profile["nodata"] = 0
+        elif nodata_type == "alpha":
+            src_profile["count"] = nband + 1
+
+        gdal_config = dict(
+            GDAL_NUM_THREADS="ALL_CPUS",
+            GDAL_TIFF_INTERNAL_MASK=True,
+            GDAL_TIFF_OVR_BLOCKSIZE="128",
+        )
+
+        with MemoryFile() as memfile:
+            with memfile.open(**src_profile) as mem:
+                ci = [ColorInterp.gray]
+                if nband > 1:
+                    ci += [ColorInterp.undefined] * (nband - 1)
+
+                if nodata_type == "alpha":
+                    data = numpy.concatenate([arr, mask])
+                    ci += [ColorInterp.alpha]
+                else:
+                    data = arr
+
+                mem.colorinterp = ci
+                mem.write(data)
+
+                cog_translate(
+                    mem,
+                    fout,
+                    output_profile,
+                    config=gdal_config,
+                    in_memory=True,
+                    dtype=dtype,
+                    quiet=True,
+                    add_mask=True if nodata_type == "mask" else False,
+                )
+
         return fout
 
-    profile_options = {"blockxsize": tilesize, "blockysize": tilesize}
-    output_profile = cog_profiles.get("deflate")
-    output_profile.update(profile_options)
-
-    arr = numpy.random.randint(1, 255, size=(nband, y_size, x_size)).astype(numpy.uint8)
-    arr[:, 0:500, 0:500] = 0
-
-    mask = numpy.zeros((1, y_size, x_size), dtype=numpy.uint8) + 255
-    mask[:, 0:500, 0:500] = 0
-
-    w, s, e, n = bounds
-    src_profile = dict(
-        driver="GTiff",
-        count=nband,
-        dtype="uint8",
-        height=y_size,
-        width=x_size,
-        crs=crs,
-        transform=from_bounds(w, s, e, n, x_size, y_size),
-    )
-    if nodata_type in ["nodata", "mask"]:
-        src_profile["nodata"] = 0
-    elif nodata_type == "alpha":
-        src_profile["count"] = nband + 1
-
-    gdal_config = dict(
-        GDAL_NUM_THREADS="ALL_CPUS",
-        GDAL_TIFF_INTERNAL_MASK=True,
-        GDAL_TIFF_OVR_BLOCKSIZE="128",
-    )
-
-    with MemoryFile() as memfile:
-        with memfile.open(**src_profile) as mem:
-            ci = [ColorInterp.gray]
-            if nband > 1:
-                ci += [ColorInterp.undefined] * (nband - 1)
-
-            if nodata_type == "alpha":
-                data = numpy.concatenate([arr, mask])
-                ci += [ColorInterp.alpha]
-            else:
-                data = arr
-
-            mem.colorinterp = ci
-            mem.write(data)
-
-            cog_translate(
-                mem,
-                fout,
-                output_profile,
-                config=gdal_config,
-                in_memory=True,
-                dtype=dtype,
-                quiet=True,
-                add_mask=True if nodata_type == "mask" else False,
-            )
-
-    return fout
-
-
-@pytest.fixture()
-def benchmark_fixtures():
-    """Create benchmark fixtures."""
-    from .benchmarks import benchmark_dataset
-
-    print("Creating Benchmark dataset...")
-    databench_dir = os.path.join(os.path.dirname(__file__), "benchmarks", "data")
-
-    if not os.path.isdir(databench_dir):
-        os.mkdir(databench_dir)
-
-    for dataset in benchmark_dataset:
-        create_cog(databench_dir, **dataset)
-
-    return True
+    return _cloudoptimized_geotiff
