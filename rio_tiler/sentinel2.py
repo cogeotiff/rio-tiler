587d2c20350531db7d54639789645e862c99e236
vincentsarago <vincent.sarago@gmail.com>
2020-02-18 10:52:20 -0500

    adapt for new sentinel L2A level

--- rio_tiler/sentinel2.py
+++ rio_tiler/sentinel2.py
@@ -2,6 +2,8 @@
 
 import os
 import re
+import warnings
+import itertools
 import multiprocessing
 from functools import partial
 from concurrent import futures
@@ -14,64 +16,81 @@ from rasterio.crs import CRS
 from rasterio.warp import transform_bounds
 
 from rio_tiler import utils
-from rio_tiler.errors import TileOutsideBounds, InvalidBandName, InvalidSentinelSceneId
+from rio_tiler.errors import (
+    DeprecationWarning,
+    TileOutsideBounds,
+    InvalidBandName,
+    InvalidSentinelSceneId,
+)
 
-SENTINEL_BUCKET = "s3://sentinel-s2-l1c"
-SENTINEL_BANDS = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "8A",
-    "09",
-    "10",
-    "11",
-    "12",
-]
+AWS_SENTINEL_BUCKET = "s3://sentinel-s2-"
+
+SENTINEL_L1_BANDS = {
+    "10m": ["02", "03", "04", "08"],
+    "20m": ["05", "06", "07", "11", "12", "8A"],
+    "60m": ["01", "09", "10"],
+}
+
+SENTINEL_L2_BANDS = {
+    "10m": ["02", "03", "04", "08"],
+    "20m": ["02", "03", "04", "05", "06", "07", "08", "11", "12", "8A"],
+    "60m": ["01", "02", "03", "04", "05", "06", "07", "08", "09", "11", "12", "8A"],
+}
+
+SENTINEL_L2_PRODUCTS = {
+    "10m": ["AOT", "WVP"],
+    "20m": ["AOT", "SCL", "WVP"],
+    "60m": ["AOT", "SCL", "WVP"],
+}
 
 # ref: https://docs.python.org/3/library/concurrent.futures.html#threadpoolexecutor
 MAX_THREADS = int(os.environ.get("MAX_THREADS", multiprocessing.cpu_count() * 5))
 
 
 def _sentinel_parse_scene_id(sceneid):
-    """Parse Sentinel-2 scene id.
+    """
+    Parse Sentinel-2 scene id.
 
     Attributes
     ----------
-    sceneid : str
-        Sentinel-2 sceneid.
+        sceneid : str
+            Sentinel-2 sceneid.
 
     Returns
     -------
-    out : dict
-        dictionary with metadata constructed from the sceneid.
+        out : dict
+            dictionary with metadata constructed from the sceneid.
 
-        e.g:
-        _sentinel_parse_scene_id('S2A_tile_20170323_07SNC_0')
-        {
-            "acquisitionDay": "23",
-            "acquisitionMonth": "03",
-            "acquisitionYear": "2017",
-            "key": "tiles/7/S/NC/2017/3/23/0",
-            "lat": "S",
-            "num": "0",
-            "satellite": "A",
-            "scene": "S2A_tile_20170323_07SNC_0",
-            "sensor": "2",
-            "sq": "NC",
-            "utm": "07",
-        }
+            e.g:
+            _sentinel_parse_scene_id('S2A_tile_20170323_07SNC_0')
+            {
+                "acquisitionDay": "23",
+                "acquisitionMonth": "03",
+                "acquisitionYear": "2017",
+                "key": "tiles/7/S/NC/2017/3/23/0",
+                "lat": "S",
+                "num": "0",
+                "satellite": "A",
+                "scene": "S2A_tile_20170323_07SNC_0",
+                "sensor": "2",
+                "sq": "NC",
+                "utm": "07",
+            }
 
     """
-
-    if not re.match("^S2[AB]_tile_[0-9]{8}_[0-9]{2}[A-Z]{3}_[0-9]$", sceneid):
+    old_sceneid = "S2[AB]_tile_[0-9]{8}_[0-9]{2}[A-Z]{3}_[0-9]"
+    new_sceneid = "S2[AB]_L[0-2][A-C]_[0-9]{8}_[0-9]{2}[A-Z]{3}_[0-9]"
+    if not re.match("^{}|{}$".format(old_sceneid, new_sceneid), sceneid):
         raise InvalidSentinelSceneId("Could not match {}".format(sceneid))
 
-    sentinel_pattern = (
+    if re.match(old_sceneid, sceneid):
+        warnings.warn(
+            "Old Sentinel-2 scene id will be deprecated starting in rio-tiler v2.0.0"
+            "Processing level is set to L1A.",
+            DeprecationWarning,
+        )
+
+    sentinel_pattern_old = (
         r"^S"
         r"(?P<sensor>\w{1})"
         r"(?P<satellite>[AB]{1})"
@@ -87,10 +106,33 @@ def _sentinel_parse_scene_id(sceneid):
         r"(?P<num>[0-9]{1})$"
     )
 
+    sentinel_pattern_new = (
+        r"^S"
+        r"(?P<sensor>\w{1})"
+        r"(?P<satellite>[AB]{1})"
+        r"_"
+        r"(?P<processingLevel>L[0-2][ABC])"
+        r"_"
+        r"(?P<acquisitionYear>[0-9]{4})"
+        r"(?P<acquisitionMonth>[0-9]{2})"
+        r"(?P<acquisitionDay>[0-9]{2})"
+        r"_"
+        r"(?P<utm>[0-9]{2})"
+        r"(?P<lat>\w{1})"
+        r"(?P<sq>\w{2})"
+        r"_"
+        r"(?P<num>[0-9]{1})$"
+    )
+
     meta = None
-    match = re.match(sentinel_pattern, sceneid, re.IGNORECASE)
-    if match:
-        meta = match.groupdict()
+    for pattern in [sentinel_pattern_old, sentinel_pattern_new]:
+        match = re.match(pattern, sceneid, re.IGNORECASE)
+        if match:
+            meta = match.groupdict()
+            break
+
+    if not meta.get("processingLevel"):
+        meta["processingLevel"] = "L1C"
 
     utm_zone = meta["utm"].lstrip("0")
     grid_square = meta["sq"]
@@ -100,13 +142,43 @@ def _sentinel_parse_scene_id(sceneid):
     day = meta["acquisitionDay"].lstrip("0")
     img_num = meta["num"]
 
-    meta["key"] = "tiles/{}/{}/{}/{}/{}/{}/{}".format(
+    meta["scene"] = sceneid
+    meta["aws_bucket"] = AWS_SENTINEL_BUCKET + meta["processingLevel"].lower()
+    meta["aws_prefix"] = "tiles/{}/{}/{}/{}/{}/{}/{}".format(
         utm_zone, latitude_band, grid_square, year, month, day, img_num
     )
+    meta["key"] = meta["aws_prefix"]  # Will be deprecated in rio-tiler v2.0.0
 
-    meta["scene"] = sceneid
+    if meta["processingLevel"] == "L1C":
+        meta["preview_file"] = "preview.jp2"
+        meta["preview_prefix"] = "preview"
+        meta["bands"] = list(
+            itertools.chain.from_iterable(
+                [bands for _, bands in SENTINEL_L1_BANDS.items()]
+            )
+        )
+        meta["valid_bands"] = meta["bands"]
+    else:
+        meta["preview_file"] = "R60m/TCI.jp2"
+        meta["preview_prefix"] = "R60m"
+        meta["bands"] = SENTINEL_L2_BANDS["60m"]
+        meta["valid_bands"] = meta["bands"] + SENTINEL_L2_PRODUCTS["60m"]
 
     return meta
+
+
+def _l2_prefixed_band(band):
+    """Return L2A prefixed bands name."""
+    if band in SENTINEL_L2_BANDS["60m"]:
+        for res, bands in SENTINEL_L2_BANDS.items():
+            if band in bands:
+                return "R{}/B{}".format(res, band)
+    elif band in SENTINEL_L2_PRODUCTS["60m"]:
+        for res, bands in SENTINEL_L2_PRODUCTS.items():
+            if band in bands:
+                return "R{}/{}".format(res, band)
+    else:
+        raise InvalidBandName("{} is not a valid Sentinel band name".format(band))
 
 
 def bounds(sceneid):
@@ -115,19 +187,22 @@ def bounds(sceneid):
 
     Attributes
     ----------
-    sceneid : str
-        Sentinel-2 sceneid.
+        sceneid : str
+            Sentinel-2 sceneid.
 
     Returns
     -------
-    out : dict
-        dictionary with image bounds.
+        out : dict
+            dictionary with image bounds.
 
     """
     scene_params = _sentinel_parse_scene_id(sceneid)
-    sentinel_address = "{}/{}".format(SENTINEL_BUCKET, scene_params["key"])
-
-    with rasterio.open("{}/preview.jp2".format(sentinel_address)) as src:
+    preview_file = os.path.join(
+        scene_params["aws_bucket"],
+        scene_params["aws_prefix"],
+        scene_params["preview_file"],
+    )
+    with rasterio.open(preview_file) as src:
         wgs_bounds = transform_bounds(
             *[src.crs, "epsg:4326"] + list(src.bounds), densify_pts=21
         )
@@ -165,27 +240,28 @@ def metadata(sceneid, pmin=2, pmax=98, **kwargs):
 
     Attributes
     ----------
-    sceneid : str
-        Sentinel-2 sceneid.
-    pmin : int, optional, (default: 2)
-        Histogram minimum cut.
-    pmax : int, optional, (default: 98)
-        Histogram maximum cut.
-    kwargs : optional
-        These are passed to 'rio_tiler.sentinel2._sentinel_stats'
-        e.g: histogram_bins=20'
+        sceneid : str
+            Sentinel-2 sceneid.
+        pmin : int, optional, (default: 2)
+            Histogram minimum cut.
+        pmax : int, optional, (default: 98)
+            Histogram maximum cut.
+        kwargs : optional
+            These are passed to 'rio_tiler.sentinel2._sentinel_stats'
+            e.g: histogram_bins=20'
 
     Returns
     -------
-    out : dict
-        Dictionary with image bounds and bands statistics.
+        out : dict
+            Dictionary with image bounds and bands statistics.
 
     """
     scene_params = _sentinel_parse_scene_id(sceneid)
-    sentinel_address = "{}/{}".format(SENTINEL_BUCKET, scene_params["key"])
+    path_prefix = os.path.join(scene_params["aws_bucket"], scene_params["aws_prefix"])
+    preview_file = os.path.join(path_prefix, scene_params["preview_file"])
 
     dst_crs = CRS({"init": "EPSG:4326"})
-    with rasterio.open("{}/preview.jp2".format(sentinel_address)) as src:
+    with rasterio.open(preview_file) as src:
         bounds = transform_bounds(
             *[src.crs, dst_crs] + list(src.bounds), densify_pts=21
         )
@@ -194,14 +270,15 @@ def metadata(sceneid, pmin=2, pmax=98, **kwargs):
     info["bounds"] = {"value": bounds, "crs": dst_crs.to_string()}
 
     addresses = [
-        "{}/preview/B{}.jp2".format(sentinel_address, band) for band in SENTINEL_BANDS
+        "{}/{}/B{}.jp2".format(path_prefix, scene_params["preview_prefix"], band)
+        for band in scene_params["bands"]
     ]
-
     _stats_worker = partial(_sentinel_stats, percentiles=(pmin, pmax), **kwargs)
     with futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
         responses = executor.map(_stats_worker, addresses)
+
     info["statistics"] = {
-        b: v for b, d in zip(SENTINEL_BANDS, responses) for k, v in d.items()
+        b: v for b, d in zip(scene_params["bands"], responses) for k, v in d.items()
     }
     return info
 
@@ -214,39 +291,42 @@ def tile(
 
     Attributes
     ----------
-    sceneid : str
-        Sentinel-2 sceneid.
-    tile_x : int
-        Mercator tile X index.
-    tile_y : int
-        Mercator tile Y index.
-    tile_z : int
-        Mercator tile ZOOM level.
-    bands : tuple, str, optional (default: ('04', '03', '02'))
-        Bands index for the RGB combination.
-    tilesize : int, optional (default: 256)
-        Output image size.
-    kwargs: dict, optional
-        These will be passed to the 'rio_tiler.utils._tile_read' function.
+        sceneid : str
+            Sentinel-2 sceneid.
+        tile_x : int
+            Mercator tile X index.
+        tile_y : int
+            Mercator tile Y index.
+        tile_z : int
+            Mercator tile ZOOM level.
+        bands : tuple, str, optional (default: ('04', '03', '02'))
+            Bands index for the RGB combination.
+        tilesize : int, optional (default: 256)
+            Output image size.
+        kwargs: dict, optional
+            These will be passed to the 'rio_tiler.utils._tile_read' function.
 
     Returns
     -------
-    data : numpy ndarray
-    mask: numpy array
+        data : numpy ndarray
+        mask: numpy array
 
     """
+    scene_params = _sentinel_parse_scene_id(sceneid)
+
     if not isinstance(bands, tuple):
         bands = tuple((bands,))
 
     for band in bands:
-        if band not in SENTINEL_BANDS:
+        if band not in scene_params["valid_bands"]:
             raise InvalidBandName("{} is not a valid Sentinel band name".format(band))
 
-    scene_params = _sentinel_parse_scene_id(sceneid)
-    sentinel_address = "{}/{}".format(SENTINEL_BUCKET, scene_params["key"])
-
-    sentinel_preview = "{}/preview.jp2".format(sentinel_address)
-    with rasterio.open(sentinel_preview) as src:
+    preview_file = os.path.join(
+        scene_params["aws_bucket"],
+        scene_params["aws_prefix"],
+        scene_params["preview_file"],
+    )
+    with rasterio.open(preview_file) as src:
         bounds = transform_bounds(src.crs, "epsg:4326", *src.bounds, densify_pts=21)
 
     if not utils.tile_exists(bounds, tile_z, tile_x, tile_y):
@@ -257,13 +337,21 @@ def tile(
     mercator_tile = mercantile.Tile(x=tile_x, y=tile_y, z=tile_z)
     tile_bounds = mercantile.xy_bounds(mercator_tile)
 
-    addresses = ["{}/B{}.jp2".format(sentinel_address, band) for band in bands]
+    path_prefix = os.path.join(scene_params["aws_bucket"], scene_params["aws_prefix"])
+    if scene_params["processingLevel"] == "L2A":
+        bands = [_l2_prefixed_band(b) for b in bands]
+    else:
+        bands = ["B{}".format(b) for b in bands]
 
-    _tiler = partial(
-        utils.tile_read, bounds=tile_bounds, tilesize=tilesize, nodata=0, **kwargs
-    )
+    def _read_tile(path):
+        with rasterio.open(path) as src_dst:
+            return utils.tile_read(
+                src_dst, bounds=tile_bounds, tilesize=tilesize, nodata=0, **kwargs
+            )
+
+    addresses = ["{}/{}.jp2".format(path_prefix, band) for band in bands]
     with futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-        data, masks = zip(*list(executor.map(_tiler, addresses)))
+        data, masks = zip(*list(executor.map(_read_tile, addresses)))
         mask = np.all(masks, axis=0).astype(np.uint8) * 255
 
     return np.concatenate(data), mask
