d202183e0b60433cf3373e29fb1784746beb225a
vincentsarago <vincent.sarago@gmail.com>
2020-03-31 17:00:57 -0400

    add simple COG info method

--- tests/test_io_cogeo.py
+++ tests/test_io_cogeo.py
@@ -10,6 +10,7 @@ PREFIX = os.path.join(os.path.dirname(__file__), "fixtures")
 ADDRESS = "{}/my-bucket/hro_sources/colorado/201404_13SED190110_201404_0x1500m_CL_1.tif".format(
     PREFIX
 )
+COG_TAGS = os.path.join(os.path.dirname(__file__), "fixtures", "cog_tags.tif")
 
 
 @pytest.fixture(autouse=True)
@@ -38,6 +39,26 @@ def test_bounds_valid():
     meta = cogeo.bounds(ADDRESS)
     assert meta.get("address") == ADDRESS
     assert len(meta.get("bounds")) == 4
+
+
+def test_info_valid():
+    """Should work as expected (get file info)"""
+    meta = cogeo.info(COG_TAGS)
+    assert meta.get("address") == COG_TAGS
+    assert meta.get("bounds")
+    assert meta.get("minzoom")
+    assert meta.get("maxzoom")
+    assert meta.get("band_descriptions")
+    assert meta.get("dtype") == "int16"
+    assert meta.get("colorinterp") == ["gray"]
+    assert meta.get("nodata_type") == "Nodata"
+    assert meta.get("scale")
+    assert meta.get("offset")
+    assert meta.get("band_metadata")
+    bmeta = meta.get("band_metadata")[0][1]
+    assert bmeta.get("STATISTICS_MAXIMUM")
+    assert bmeta.get("STATISTICS_MEAN")
+    assert bmeta.get("STATISTICS_MINIMUM")
 
 
 def test_metadata_valid():
