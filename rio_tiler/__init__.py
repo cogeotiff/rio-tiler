205d34012a2133202a6704d4de7f95453d81f422
Kyle Barron <kylebarron2@gmail.com>
2020-08-29 11:30:02 -0600

    Export modules from top-level package (#246)
    * Export modules from top-level package
    
    * Use black profile for isort
    
    * Bump isort version
    
    * bump isort version

--- rio_tiler/__init__.py
+++ rio_tiler/__init__.py
@@ -2,4 +2,18 @@
 
 import pkg_resources
 
+from . import (  # noqa
+    colormap,
+    constants,
+    errors,
+    expression,
+    io,
+    mercator,
+    mosaic,
+    profiles,
+    reader,
+    tasks,
+    utils,
+)
+
 version = pkg_resources.get_distribution(__package__).version
--- rio_tiler/mosaic/methods/__init__.py
+++ rio_tiler/mosaic/methods/__init__.py
@@ -1,1 +1,10 @@
 """rio-tiler.mosaic.methods: Mosaic filling methods."""
+
+from .defaults import (  # noqa
+    FirstMethod,
+    HighestMethod,
+    LowestMethod,
+    MeanMethod,
+    MedianMethod,
+    StdevMethod,
+)
