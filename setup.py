e9246b49dd3224f80eee91101d4e3b97dc271258
Vincent Sarago <vincent.sarago@gmail.com>
2020-09-08 22:36:30 -0400

    Render as numpy (#259)
    * add numpy binary render
    
    * update changelog

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extra_reqs = {
 
 setup(
     name="rio-tiler",
-    version="2.0b8",
+    version="2.0b9",
     python_requires=">=3.5",
     description="Rasterio plugin to read mercator tiles from Cloud Optimized GeoTIFF.",
     long_description=readme,
