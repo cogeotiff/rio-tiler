4bd9be5db98827e40d4222627c3af59e9063bd95
vincentsarago <vincent.sarago@gmail.com>
2021-01-27 09:52:57 -0500

    Bump version: 2.0.0rc4 → 2.0.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ extra_reqs = {
 
 setup(
     name="rio-tiler",
-    version="2.0.0rc4",
+    version="2.0.0",
     python_requires=">=3.6",
     description="Rasterio plugin to read mercator tiles from Cloud Optimized GeoTIFF.",
     long_description=readme,
