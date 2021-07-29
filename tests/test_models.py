641d5a2acfd30c9a253d8d049207e58a26d5021a
Vincent Sarago <vincent.sarago@gmail.com>
2021-07-29 19:13:50 +0200

    Fix alpha band values when storing `Uint16` data in PNG (#407)
    * Fix alpha band values when storing `Uint16` data in **PNG**
    
    * make sure we keep even distribution of alpha values

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,10 @@
 """Test rio_tiler.models."""
 
+from io import BytesIO
+
 import numpy
 import pytest
+import rasterio
 
 from rio_tiler.errors import InvalidDatatypeWarning
 from rio_tiler.models import ImageData
@@ -53,3 +56,40 @@ def test_imageData_AutoRescalingAllTypes(dtype):
         ImageData(numpy.zeros((3, 256, 256), dtype=dtype)).render(
             img_format="JP2OPENJPEG"
         )
+
+
+def test_16bit_PNG():
+    """Uint16 Mask value should be between 0 and 65535 for PNG."""
+    mask = numpy.zeros((256, 256), dtype="uint16") + 255
+    mask[0:10, 0:10] = 0
+    mask[10:11, 10:11] = 100
+
+    with pytest.warns(None):
+        img = ImageData(numpy.zeros((1, 256, 256), dtype="uint16"), mask,).render(
+            img_format="PNG"
+        )
+
+        with rasterio.open(BytesIO(img)) as src:
+            assert src.count == 2
+            assert src.meta["dtype"] == "uint16"
+            arr = src.read(2)
+            assert arr.min() == 0
+            assert arr.max() == 65535
+            assert (arr[0:10, 0:10] == 0).all()
+            assert (arr[10:11, 10:11] == 25700).all()
+            assert (arr[11:, 11:] == 65535).all()
+
+    with pytest.warns(None):
+        img = ImageData(numpy.zeros((3, 256, 256), dtype="uint16"), mask,).render(
+            img_format="PNG"
+        )
+
+        with rasterio.open(BytesIO(img)) as src:
+            assert src.count == 4
+            assert src.meta["dtype"] == "uint16"
+            arr = src.read(4)
+            assert arr.min() == 0
+            assert arr.max() == 65535
+            assert (arr[0:10, 0:10] == 0).all()
+            assert (arr[10:11, 10:11] == 25700).all()
+            assert (arr[11:, 11:] == 65535).all()
