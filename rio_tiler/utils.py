a78f3624d97de9e6afd7071e0cee6381511bda70
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2022-01-24 17:46:48 +0100

    is not!

--- rio_tiler/utils.py
+++ rio_tiler/utils.py
@@ -41,7 +41,7 @@ def aws_get_object(
     if not client:
         session = boto3_session()
         endpoint_url = os.environ.get("AWS_S3_ENDPOINT", None)
-        if ( endpoint_url != None ):
+        if ( endpoint_url is not None ):
             use_https = os.environ.get("AWS_HTTPS", "YES")
             if ( use_https == "YES" ):
                 endpoint_url = "https://" + endpoint_url
