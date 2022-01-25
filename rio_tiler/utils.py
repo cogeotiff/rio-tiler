dbe6df5590d569d82ce17c0d5ab215b423896104
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2022-01-25 09:08:53 +0100

    GDAL options comment

--- rio_tiler/utils.py
+++ rio_tiler/utils.py
@@ -40,7 +40,9 @@ def aws_get_object(
     """AWS s3 get object content."""
     if not client:
         session = boto3_session()
-        endpoint_url = os.environ.get("AWS_S3_ENDPOINT", None)
+        # AWS_S3_ENDPOINT and AWS_HTTPS are GDAL config options of vsis3 driver
+        # https://gdal.org/user/virtual_file_systems.html#vsis3-aws-s3-files
+        endpoint_url = os.environ.get("AWS_S3_ENDPOINT", None) 
         if endpoint_url is not None:
             use_https = os.environ.get("AWS_HTTPS", "YES")
             if use_https == "YES":
