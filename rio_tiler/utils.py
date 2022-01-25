92010576bc2e7b1a4a4554f0a8850e8ab8f1f18f
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2022-01-25 09:27:20 +0100

    black reformatting

--- rio_tiler/utils.py
+++ rio_tiler/utils.py
@@ -42,7 +42,7 @@ def aws_get_object(
         session = boto3_session()
         # AWS_S3_ENDPOINT and AWS_HTTPS are GDAL config options of vsis3 driver
         # https://gdal.org/user/virtual_file_systems.html#vsis3-aws-s3-files
-        endpoint_url = os.environ.get("AWS_S3_ENDPOINT", None) 
+        endpoint_url = os.environ.get("AWS_S3_ENDPOINT", None)
         if endpoint_url is not None:
             use_https = os.environ.get("AWS_HTTPS", "YES")
             if use_https.upper() in ["YES", "TRUE", "ON"]:
